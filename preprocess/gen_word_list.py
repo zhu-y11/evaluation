f8016b8fccf0d24c5427408216a242a7082e5236
zhu-y11 <zhu-y11@foxmail.com>
2018-10-02 23:26:51 +0100

    minor change

--- preprocess/gen_word_list.py
+++ preprocess/gen_word_list.py
@@ -2,7 +2,7 @@
 #!/usr/bin/python3
 """
 @Author Yi Zhu
-Upated  04/24/2018
+Upated  09/22/2018
 Generate word list
 """
 
@@ -13,8 +13,11 @@ import sys
 import os
 import codecs
 
+import pdb
+
 data_dir = sys.argv[1]
 word_set = []
+# for turkish 
 #with codecs.open(in_file, 'r', encoding = 'latin-1') as fin:
 for root, dirs, files in os.walk(data_dir):
   for f in files:
@@ -23,6 +26,9 @@ for root, dirs, files in os.walk(data_dir):
     with open(os.path.join(root, f), 'r') as fin:
       for line in fin:
         linevec = line.strip().lower().split()[:2]
+        if os.path.basename(f).startswith('7.card660.en'):
+          linevec = [w.strip().split('_') for w in linevec]
+          linevec = [w for w_seq in linevec for w in w_seq]
         word_set.extend(linevec)
 word_set = list(set(word_set))
 print('\n'.join(word_set))
